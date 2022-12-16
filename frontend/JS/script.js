9be6bbe7b7b4345c674620483df84478267e56c9
Olúbùsọ́lá Odunuga <olubusolao3@gmail.com>
2022-12-16 01:16:05 +0100

    Sign up and Log in update

--- frontend/JS/script.js
+++ frontend/JS/script.js
@@ -1,79 +1,69 @@
-// const baseUrl = 'https://example.com'
-const baseUrl =  'http://localhost:3000'
+const baseUrl = "http://localhost:3000";
+
+const form = document.getElementById('form');
+const username = document.getElementById('username');
+const email = document.getElementById('email');
+const password = document.getElementById('password');
 
 
-const signUpBtn = document.querySelector('.sign-up-btn')
+form.addEventListener('submit', e => {
+    e.preventDefault();
 
-let user={
-    name:'',
-    email:'',
-    password:''
+    validateInputs();
+});
+
+const setError = (element, message) => {
+    const inputControl = element.parentElement;
+    const errorDisplay = inputControl.querySelector('.error');
+
+    errorDisplay.innerText = message;
+    inputControl.classList.add('error');
+    inputControl.classList.remove('success')
 }
 
-document.getElementById('name').addEventListener('change', () => {
-    user.name = document.getElementById('name').value.trim('')
-})
-document.getElementById('email').addEventListener('change', () => {
-    user.email = document.getElementById('email').value.trim('')
-})
-document.getElementById('password').addEventListener('change', () => {
-    user.password = document.getElementById('password').value.trim('')
-})
+const setSuccess = element => {
+    const inputControl = element.parentElement;
+    const errorDisplay = inputControl.querySelector('.error');
 
-let payload = {
-    method: 'POST',
-    headers: {
-        'Content-Type':
-            'application/json;charset=utf-8'
-    },
-    body: JSON.stringify(user)
+    errorDisplay.innerText = '';
+    inputControl.classList.add('success');
+    inputControl.classList.remove('error');
+};
+
+const isValidEmail = email => {
+    const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
 }
 
-const getSignup = async () => {
-    try{ const response = await fetch(`${baseUrl}/user/signup`, payload)
-    const data = await response.json()
-    console.log(data)} catch (err){
-        console.log(err)
-    } 
-}
-signUpBtn.addEventListener('click', (e) => {
-    e.preventDefault()
-    getSignup()
-    window.location.pathname = '/index.html'
-})
+
+const validateInputs = () => {
+    const usernameValue = username.value.trim();
+    const emailValue = email.value.trim();
+    const passwordValue = password.value.trim();
 
 
-    
-function login(event) {
-    event.preventDefault()
-    let emailAddress = document.getElementById('email').value
-    let password = document.getElementById('password').value
-
-    if ((emailAddress !== '' && emailAddress !== undefined && emailAddress !== null) &&
-        (password !== '' && password !== undefined !== password !== null)) {
-        // call api here
-
-        let user = {
-            email: emailAddress,
-            password: password
-        }
-
-        let payload = {
-            method: 'POST',
-            headers: {
-                'Content-Type':
-                    'application/json;charset=utf-8'
-            },
-            body: JSON.stringify(user)
-        }
-
-        fetch(`${baseUrl}/user/login`, payload)
-            .then(response => {
-                let resData = response.json()
-
-
-            })
-            .then(data => console.log(data));
-
+    if(usernameValue === '') {
+        setError(username, 'Username is required');
+    } else {
+        setSuccess(username);
     }
+
+
+if(emailValue === '') {
+    setError(email, 'Email is required');
+} else if (!isValidEmail(emailValue)) {
+    setError(email, 'Please Provide a valid email address');
+} else {
+    setSuccess(email);
+}
+
+
+if(passwordValue === '') {
+    setError(password, 'Password is required');
+} else if (passwordValue.length < 8 ) {
+    setError(password, 'Password must be at least 8 character.')
+} else {
+    setSuccess(password);
+}
+
 }
